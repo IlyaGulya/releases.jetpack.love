83a107268145f941534dee1a8e572fce5f6de125
Ilya Gulya <ilya@gulya.me>
2024-12-09 20:35:39 +0500

    add posthog privacy-friendly analytics

--- packages/spa/src/main.tsx
+++ packages/spa/src/main.tsx
@@ -1,13 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Initialize PostHog once
+if (!posthog.__loaded) {
+  posthog.init(
+    import.meta.env.VITE_PUBLIC_POSTHOG_KEY,
+    {
+      api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+      loaded: (posthog) => {
+        if (import.meta.env.DEV) posthog.debug()
+      }
+    }
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PostHogProvider client={posthog}>
+        <App />
+      </PostHogProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
